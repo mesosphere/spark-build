a36912effe27a9d13ba6c2afd2b2d579a05134cd
Sergiusz Urbaniak <sur@mesosphere.io>
2016-02-29 15:56:21 +0100

    remove debug entries

--- docker/runit/init.sh
+++ docker/runit/init.sh
@@ -8,9 +8,6 @@ export HISTORY_SERVER_PORT="${PORT2}"
 export SPARK_PROXY_PORT="${PORT3}"
 export WEBUI_URL="http://${FRAMEWORK_NAME}${DNS_SUFFIX}:${SPARK_PROXY_PORT}"
 
-# TODO(sur) remove, debug only
-env | sort
-
 if [ "${ENABLE_HISTORY_SERVER:=false}" = "true" ]; then
     ln -s /var/lib/runit/service/history-server /etc/service/history-server
 fi
