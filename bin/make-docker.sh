45c350d5b06dc77005f3183934cb59d96aeac570
Michael Gummelt <mgummelt@mesosphere.io>
2016-03-10 10:51:52 -0800

    add conf files

--- bin/make-docker.sh
+++ bin/make-docker.sh
@@ -5,8 +5,9 @@
 
 rm -rf build/docker
 mkdir -p build/docker
-cp -r "$1/." build/docker
-cp docker/* build/docker
+cp -r "$1/." build/docker/dist
+cp -r conf/* build/docker/dist/conf
+cp -r docker/* build/docker
 
 pushd build/docker
 docker build -t $2 .
