919bdbef00986cdf4167d6f7a7efcd3fe245b66b
Sergiusz Urbaniak <sur@mesosphere.io>
2016-03-14 10:57:40 +0100

    dispatcher/history: use FRAMEWORK_NAME in web proxy base settings

--- docker/runit/init.sh
+++ docker/runit/init.sh
@@ -7,6 +7,8 @@ export DISPATCHER_UI_PORT="${PORT1}"
 export HISTORY_SERVER_PORT="${PORT2}"
 export SPARK_PROXY_PORT="${PORT3}"
 export WEBUI_URL="http://${FRAMEWORK_NAME}${DNS_SUFFIX}:${SPARK_PROXY_PORT}"
+export HISTORY_SERVER_WEB_PROXY_BASE="/service/${FRAMEWORK_NAME}/history"
+export DISPATCHER_UI_WEB_PROXY_BASE="/service/${FRAMEWORK_NAME}/ui"
 
 if [ "${ENABLE_HISTORY_SERVER:=false}" = "true" ]; then
     ln -s /var/lib/runit/service/history-server /etc/service/history-server
