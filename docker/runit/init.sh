97fa75cccb7d9334c69082a455c4689c319ee8a4
Sergiusz Urbaniak <sur@mesosphere.io>
2016-03-14 11:56:26 +0100

    dispatcher: configure /dispatcher as root URL

--- docker/runit/init.sh
+++ docker/runit/init.sh
@@ -8,7 +8,7 @@ export HISTORY_SERVER_PORT="${PORT2}"
 export SPARK_PROXY_PORT="${PORT3}"
 export WEBUI_URL="http://${FRAMEWORK_NAME}${DNS_SUFFIX}:${SPARK_PROXY_PORT}"
 export HISTORY_SERVER_WEB_PROXY_BASE="/service/${FRAMEWORK_NAME}/history"
-export DISPATCHER_UI_WEB_PROXY_BASE="/service/${FRAMEWORK_NAME}/ui"
+export DISPATCHER_UI_WEB_PROXY_BASE="/service/${FRAMEWORK_NAME}/dispatcher"
 
 if [ "${ENABLE_HISTORY_SERVER:=false}" = "true" ]; then
     ln -s /var/lib/runit/service/history-server /etc/service/history-server
