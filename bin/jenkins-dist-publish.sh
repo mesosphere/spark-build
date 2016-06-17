b2256e9c84979a7c793db1ffaa21d08f49dfbcf8
Michael Gummelt <mgummelt@mesosphere.io>
2016-06-17 12:29:15 -0700

    dev prod aws access keys

--- bin/jenkins-dist-publish.sh
+++ bin/jenkins-dist-publish.sh
@@ -2,6 +2,8 @@
 
 export S3_BUCKET=downloads.mesosphere.io
 export S3_PREFIX=spark/assets/
+export AWS_ACCESS_KEY_ID=${PROD_AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${PROD_SECRET_ACCESS_KEY}
 
 source spark-build/bin/jenkins.sh
 
--- bin/jenkins-dist-test.sh
+++ bin/jenkins-dist-test.sh
@@ -2,6 +2,8 @@
 
 export S3_BUCKET=spark-build
 export S3_PREFIX=
+export AWS_ACCESS_KEY_ID=${DEV_AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${DEV_SECRET_ACCESS_KEY}
 export DOCKER_IMAGE=mesosphere/spark-dev:${GIT_COMMIT}
 
 source spark-build/bin/jenkins.sh
