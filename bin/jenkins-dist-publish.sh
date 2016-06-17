3b7ab789d689669cfd0288e0513b7dcdd2fe6501
Michael Gummelt <mgummelt@mesosphere.io>
2016-06-17 13:43:35 -0700

    fix aws key name

--- bin/jenkins-dist-publish.sh
+++ bin/jenkins-dist-publish.sh
@@ -3,7 +3,7 @@
 export S3_BUCKET=downloads.mesosphere.io
 export S3_PREFIX=spark/assets/
 export AWS_ACCESS_KEY_ID=${PROD_AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${PROD_SECRET_ACCESS_KEY}
+export AWS_SECRET_ACCESS_KEY=${PROD_AWS_SECRET_ACCESS_KEY}
 
 source spark-build/bin/jenkins.sh
 
