9c699cf0d01ec5e2c0536d09406e46c0e64b2535
Michael Gummelt <mgummelt@mesosphere.io>
2016-06-16 17:19:35 -0700

    jenkins functions

--- bin/jenkins-test.sh
+++ bin/jenkins-test.sh
@@ -10,6 +10,8 @@ export DOCKER_IMAGE=mesosphere/spark-dev:${GIT_COMMIT}
 
 function make_distribution {
     # ./build/sbt assembly
+    pushd spark
+    
     if [ -f make-distribution.sh ]; then
         ./make-distribution.sh -Phadoop-2.4 -DskipTests
     else
@@ -25,21 +27,31 @@ function make_distribution {
     local DIST="spark-${GIT_COMMIT}"
     mv dist ${DIST}
     tar czf ${DIST}.tgz ${DIST}
+
+    popd
 }
 
 function upload_to_s3 {
+    pushd spark
+    
     aws s3 cp \
         --acl public-read \
         spark-*.tgz \
         "s3://${S3_BUCKET}/${S3_PREFIX}"
+
+    popd
 }
 
 function update_manifest {
-    # update manifest.json
+    pushd spark-build
+    
+    # update manifest.json    
     SPARK_DIST=$(ls ../spark/spark*.tgz)
     SPARK_URI="http://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}$(basename ${SPARK_DIST})"
     cat manifest.json | jq ".spark_uri=\"${SPARK_URI}\"" > manifest.json.tmp
     mv manifest.json.tmp manifest.json
+
+    popd
 }
 
 function install_cli {
@@ -56,35 +68,36 @@ function docker_login {
     docker login --username="${DOCKER_USERNAME}" --password="${DOCKER_PASSWORD}"
 }
 
-function main {
-    # prereqs
-    #pip install virtualenv httpie
-    #curl -LO https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.tgz
-    #tar xvf sbt-0.13.9.tgz
-    #export PATH=$(pwd)/sbt/bin:$PATH
-
-    docker_login
-
-    pushd spark
-    make_distribution
-    upload_to_s3
-    popd
-
-    pushd spark-build
-
-    update_manifest
+function spark_test {
     install_cli
-
+    
+    pushd spark-build
+    docker_login    
     make docker
-
     CLUSTER_NAME=spark-package-${BUILD_NUMBER} \
                 TEST_RUNNER_DIR=$(pwd)/../mesos-spark-integration-tests/test-runner/ \
                 DCOS_CHANNEL=testing/continuous \
                 DCOS_USERNAME=bootstrapuser \
                 DCOS_PASSWORD=deleteme \
                 make test
-
     popd
+}
+
+function upload_distribution {
+    make_distribution
+    upload_to_s3    
+    update_manifest
+}
+
+function main {
+    # prereqs
+    #pip install virtualenv httpie
+    #curl -LO https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.tgz
+    #tar xvf sbt-0.13.9.tgz
+    #export PATH=$(pwd)/sbt/bin:$PATH
+    
+    upload_distribution
+    spark_test    
 }
 
 main
