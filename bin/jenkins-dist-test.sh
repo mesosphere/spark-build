eff4fb0e15577525e7e94674e33259fcfe94ae7e
Michael Gummelt <mgummelt@mesosphere.io>
2016-07-11 13:33:59 -0700

    aws env vars

--- bin/jenkins-dist-test.sh
+++ bin/jenkins-dist-test.sh
@@ -2,8 +2,6 @@
 
 export S3_BUCKET=infinity-artifacts
 export S3_PREFIX=spark
-export AWS_ACCESS_KEY_ID=${DEV_AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${DEV_SECRET_ACCESS_KEY}
 export DOCKER_IMAGE=mesosphere/spark-dev:${GIT_COMMIT}
 
 source spark-build/bin/jenkins.sh
