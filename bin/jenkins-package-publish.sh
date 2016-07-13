d29ceedaee751635e47f32e56415d24537ef3f68
Michael Gummelt <mgummelt@mesosphere.io>
2016-07-13 16:29:32 -0700

    revert bucket

--- bin/jenkins-package-publish.sh
+++ bin/jenkins-package-publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+export VERSION=${GIT_BRANCH#refs/tags/}
 export S3_BUCKET=infinity-artifacts
 export S3_PREFIX=spark/
-export DOCKER_IMAGE=mesosphere/spark:${GIT_BRANCH#refs/tags/}
+export DOCKER_IMAGE=mesosphere/spark:${VERSION}
 
 source spark-build/bin/jenkins.sh
 
