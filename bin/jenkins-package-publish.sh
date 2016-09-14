7976f01637c97cc10728b89c87b3eb9302bfe92b
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-14 15:29:07 -0700

    remove trailing slash

--- bin/jenkins-package-publish.sh
+++ bin/jenkins-package-publish.sh
@@ -2,9 +2,9 @@
 
 export VERSION=${GIT_BRANCH#refs/tags/}
 export S3_BUCKET=downloads.mesosphere.io
-export S3_PREFIX=spark/assets/
+export S3_PREFIX=spark/assets
 export DEV_S3_BUCKET=infinity-artifacts
-export DEV_S3_PREFIX=autodelete7d/spark/
+export DEV_S3_PREFIX=autodelete7d/spark
 export DOCKER_IMAGE=mesosphere/spark:${VERSION}
 
 source spark-build/bin/jenkins.sh
