ff3661771b4d6531cbdc6b943f1bda86e1428d8e
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-26 16:12:12 -0700

    docker login

--- bin/jenkins-dist-test.sh
+++ bin/jenkins-dist-test.sh
@@ -5,13 +5,15 @@ set -e -x -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SPARK_BUILD_DIR=${DIR}/..
 
+function run() {
+    source bin/jenkins.sh
+    install_cli
+    docker_login
+    make dist && export $(cat spark_dist_uri.properties)
+    make universe && export $(cat "${WORKSPACE}/stub-universe.properties")
+    make test
+}
+
 pushd "${SPARK_BUILD_DIR}"
-
-source bin/jenkins.sh
-
-install_cli
-make dist && export $(cat spark_dist_uri.properties)
-make universe && export $(cat "${WORKSPACE}/stub-universe.properties")
-make test
-
+run
 popd
