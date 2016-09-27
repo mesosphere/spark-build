c406688d636ca0238f90977c22ff92660ca3b823
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-27 15:59:29 -0700

    fix local

--- bin/jenkins-dist-publish.sh
+++ bin/jenkins-dist-publish.sh
@@ -9,6 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SPARK_BUILD_DIR=${DIR}/..
 
 pushd "${SPARK_BUILD_DIR}"
-local VERSION=${GIT_BRANCH#refs/tags/custom-}
+VERSION=${GIT_BRANCH#refs/tags/custom-}
 DIST_NAME="spark-${VERSION}" make dist
 popd
