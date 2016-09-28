9d464da7503bcaf2ccb0d3abed0a0eaf66b0e830
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-28 15:48:16 -0700

    fix error message

--- bin/dist.sh
+++ bin/dist.sh
@@ -4,12 +4,15 @@
 # containing the URL of the distribution.
 #
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SPARK_DIR="${DIR}/../../spark"
+
 check_env() {
     # Check env early, before starting the cluster:
     if [ -z "$S3_BUCKET" \
             -o -z "$S3_PREFIX" \
             -o -z "$DIST_NAME" ]; then
-        echo "Missing required env. See check in ${BIN_DIR}/test.sh."
+        echo "Missing required env. See check in ${BIN_DIR}/dist.sh."
         env
         exit 1
     fi
@@ -20,9 +23,6 @@ function write_properties() {
     SPARK_DIST_URI="http://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}/${SPARK_FILENAME}"
     echo "SPARK_DIST_URI=${SPARK_DIST_URI}" > spark_dist_uri.properties
 }
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SPARK_DIR="${DIR}/../../spark"
 
 check_env
 source "${DIR}/jenkins.sh"
