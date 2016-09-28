a9b7bf415cfe2415622083f49b0984d848d23cfa
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-28 15:47:40 -0700

    make DIST_NAME env var required in make dist

--- bin/dist.sh
+++ bin/dist.sh
@@ -3,24 +3,29 @@
 # Builds spark, and creates a properties file spark_dist_uri.properties
 # containing the URL of the distribution.
 #
-# Env vars:
-#   S3_BUCKET
-#   S3_PREFIX
-#   AWS_ACCESS_KEY_ID
-#   AWS_SECRET_ACCESS_KEY
-#   GIT_COMMIT
 
-set -e -x -o pipefail
+check_env() {
+    # Check env early, before starting the cluster:
+    if [ -z "$S3_BUCKET" \
+            -o -z "$S3_PREFIX" \
+            -o -z "$DIST_NAME" ]; then
+        echo "Missing required env. See check in ${BIN_DIR}/test.sh."
+        env
+        exit 1
+    fi
+}
+
+function write_properties() {
+    SPARK_FILENAME=$(basename $(ls ${SPARK_DIR}/spark*.tgz))
+    SPARK_DIST_URI="http://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}/${SPARK_FILENAME}"
+    echo "SPARK_DIST_URI=${SPARK_DIST_URI}" > spark_dist_uri.properties
+}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SPARK_DIR="${DIR}/../../spark"
 
+check_env
 source "${DIR}/jenkins.sh"
-
-GIT_COMMIT_NAME="spark-${GIT_COMMIT}"
-DIST_NAME=${DIST_NAME:-$GIT_COMMIT_NAME} make_distribution
+make_distribution
 upload_to_s3
-
-SPARK_FILENAME=$(basename $(ls ${SPARK_DIR}/spark*.tgz))
-SPARK_DIST_URI="http://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}/${SPARK_FILENAME}"
-echo "SPARK_DIST_URI=${SPARK_DIST_URI}" > spark_dist_uri.properties
+write_properties
