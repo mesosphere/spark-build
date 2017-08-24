99d6c378ae9e7c83eb62ea03cc80026d5a2e3747
Susan X. Huynh <xhuynh@mesosphere.com>
2017-08-24 11:44:21 -0700

    Fixed soak terasort test after refactor. (#173)

--- tests/test_soak.py
+++ tests/test_soak.py
@@ -10,6 +10,7 @@ import utils
 
 
 LOGGER = logging.getLogger(__name__)
+SOAK_SPARK_APP_NAME='/spark'
 TERASORT_JAR='https://downloads.mesosphere.io/spark/examples/spark-terasort-1.0-jar-with-dependencies_2.11.jar'
 TERASORT_MAX_CORES=6
 
@@ -30,29 +31,32 @@ def test_terasort():
 def _run_teragen():
     jar_url = TERASORT_JAR
     input_size = os.getenv('TERASORT_INPUT_SIZE', '1g')
-    utils.run_tests(jar_url,
-               "{} hdfs:///terasort_in".format(input_size),
-               "Number of records written",
-               ["--class", "com.github.ehiggs.spark.terasort.TeraGen",
-                "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
+    utils.run_tests(app_url=jar_url,
+                    app_args="{} hdfs:///terasort_in".format(input_size),
+                    expected_output="Number of records written",
+                    app_name=SOAK_SPARK_APP_NAME,
+                    args=["--class", "com.github.ehiggs.spark.terasort.TeraGen",
+                          "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
 
 
 def _run_terasort():
     jar_url = TERASORT_JAR
-    utils.run_tests(jar_url,
-               "hdfs:///terasort_in hdfs:///terasort_out",
-               "",
-               ["--class", "com.github.ehiggs.spark.terasort.TeraSort",
-                "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
+    utils.run_tests(app_url=jar_url,
+                    app_args="hdfs:///terasort_in hdfs:///terasort_out",
+                    expected_output="",
+                    app_name=SOAK_SPARK_APP_NAME,
+                    args=["--class", "com.github.ehiggs.spark.terasort.TeraSort",
+                          "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
 
 
 def _run_teravalidate():
     jar_url = TERASORT_JAR
-    utils.run_tests(jar_url,
-               "hdfs:///terasort_out hdfs:///terasort_validate",
-               "partitions are properly sorted",
-               ["--class", "com.github.ehiggs.spark.terasort.TeraValidate",
-                "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
+    utils.run_tests(app_url=jar_url,
+                    app_args="hdfs:///terasort_out hdfs:///terasort_validate",
+                    expected_output="partitions are properly sorted",
+                    app_name=SOAK_SPARK_APP_NAME,
+                    args=["--class", "com.github.ehiggs.spark.terasort.TeraValidate",
+                          "--conf", "spark.cores.max={}".format(TERASORT_MAX_CORES)])
 
 
 def _delete_hdfs_terasort_files():
