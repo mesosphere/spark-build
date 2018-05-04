3041f446d95ea3932d4726ba896a10b1faaf3f07
Susan X. Huynh <xhuynh@mesosphere.com>
2018-05-04 14:29:37 -0700

    fix parsing of input file

--- scale-tests/scale_test.py
+++ scale-tests/scale_test.py
@@ -56,7 +56,7 @@ def submit_loop(launch_rate_per_min, dispatchers):
 
     dispatcher_index = 0
     while(True):
-        service_name,driver_role = dispatchers[dispatcher_index].split(",")
+        service_name,driver_role = dispatchers[dispatcher_index]
         t = Thread(target=submit_job, args=(service_name, driver_role))
         t.start()
         dispatcher_index = (dispatcher_index + 1) % num_dispatchers
