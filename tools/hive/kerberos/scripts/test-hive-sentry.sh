a78673706c47711c242a87731a1663b886b18394
Susan X. Huynh <xhuynh@mesosphere.com>
2018-08-30 08:54:39 -0700

    Revert "Removed the test script."
    This reverts commit ccbd2a7dd1fc7be05db3944ff5464a61eed359d2.

---
+++ tools/hive/kerberos/scripts/test-hive-sentry.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+# Create a user "alice" since Sentry authorization relies on the Linux user and group information
+useradd alice
+
+# Grant permissions to user “alice”
+echo "Grant permissions to user alice ..."
+kdestroy
+kinit -k -t /usr/local/hadoop/etc/hadoop/hdfs.keytab hive/${HOSTNAME}@LOCAL
+cat <<EOF >grant_alice.sql
+CREATE ROLE test_role;
+GRANT ROLE test_role to GROUP alice;
+GRANT ROLE test_role to GROUP root;
+GRANT ALL on DATABASE default to ROLE test_role WITH GRANT OPTION;
+EOF
+beeline -u "jdbc:hive2://localhost:10000/default;principal=hive/${HOSTNAME}@LOCAL" -f grant_alice.sql
+
+# Log back in as hdfs
+kdestroy
+kinit -k -t /usr/local/hadoop/etc/hadoop/hdfs.keytab hdfs@LOCAL
