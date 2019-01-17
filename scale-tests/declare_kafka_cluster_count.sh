2184e6e922560b5f3d98813b0eaf8060ba78cd9d
Murilo Pereira <murilo@murilopereira.com>
2019-01-17 10:39:11 +0100

    [DCOS-45256] Allow Prometheus service name to be configured by top-level scripts. (#474)

--- scale-tests/declare_kafka_cluster_count.sh
+++ scale-tests/declare_kafka_cluster_count.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+readonly PROMETHEUS_SERVICE="${PROMETHEUS_SERVICE:-prometheus}"
+
 if [[ $1 != ${1//[^0-9]/} || $1 == 0 ]]; then
   echo "First argument must be a positive integer." >&2
   exit 1
@@ -8,6 +10,7 @@ fi
 value="$1"
 shift
 exec "$(dirname "$0")"/push_metric.sh \
+  --prometheus-service "${PROMETHEUS_SERVICE}" \
   --metric-name "set_kafka_or_zk_cluster_count" \
   --metric-description "Expected number of kafka and zookeeper clusters." \
   --metric-value "${value}" \
--- scale-tests/declare_test_phase.sh
+++ scale-tests/declare_test_phase.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+readonly PROMETHEUS_SERVICE="${PROMETHEUS_SERVICE:-prometheus}"
+
 case "$1" in
 'start'|'started'|'steady')
   code="1"
@@ -20,6 +22,7 @@ stop*|'starting'|'end')
 esac
 shift
 exec "$(dirname "$0")"/push_metric.sh \
+  --prometheus-service "${PROMETHEUS_SERVICE}" \
   --metric-name "test_status" \
   --metric-description "Status of the test (0==not running, 1==in steady state, 2==fault injection)." \
   --metric-value "${code}" \
