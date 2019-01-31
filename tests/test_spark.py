3e6f97e9221ef4bce8d18cd939975b46a15bb33f
Sam Tran <stran@mesosphere.com>
2019-01-31 15:11:19 -0800

    Add exponential backoff to unique VIPs (#482)
    Looks a bit flaky and 6s may not be enough time for the dispatcher to
    get up and running

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -350,7 +350,7 @@ def test_driver_executor_tls():
 @pytest.mark.sanity
 def test_unique_vips():
 
-    @retrying.retry(stop_max_attempt_number=3, wait_fixed=2000)
+    @retrying.retry(wait_exponential_multiplier=1000, stop_max_attempt_number=7) # ~2 minutes
     def verify_ip_is_reachable(ip):
         ok, _ = sdk_cmd.master_ssh("curl -v {}".format(ip))
         assert ok
